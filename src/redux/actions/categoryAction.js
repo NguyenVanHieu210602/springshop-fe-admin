82f85cafa03f7532e0154f22e6b8f482d20d079c
Hieu <vanhieu210602@gmail.com>
2023-05-10 10:13:01 +0700

    add insert category functions

--- src/redux/actions/categoryAction.js
+++ src/redux/actions/categoryAction.js
@@ -1,8 +1,22 @@
+import CategoryService from "../../services/categoryService";
+import { CATEGORY_SET } from "./actionTypes";
+
 export const insertCategory = (category, navigate) => async (dispatch) => {
-  // const sever = new CategoryService()
+  const service = new CategoryService();
 
   try {
     console.log("insert category");
+
+    const response = await service.insertCategory(category);
+
+    if (response.status === 201) {
+      dispatch({
+        type: CATEGORY_SET,
+        payload: response.data,
+      });
+    }
+
+    console.log(response);
   } catch (error) {
     console.log("Error" + error);
   }
